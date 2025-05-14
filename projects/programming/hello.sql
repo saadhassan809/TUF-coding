ca8daea42f5f8fec5942c08c4d40a781961d87e7
Saad hassan <156897531+saadhassan809@users.noreply.github.com>
2025-05-14 14:51:52 +0100

    Add files via upload

---
+++ projects/programming/hello.sql
@@ -0,0 +1,5 @@
+<form action="service.html" method="file">
+  <label for="name">Name:</label><br>
+  <input type="text" id="name" name="name"><br>
+  <input type="submit" value="Submit">
+</form>
---
+++ projects/programming/project.js
@@ -0,0 +1,5 @@
+alert ("Hello to your dream website");
+
+//this is a comment 
+/* this is also a comment */
+var x =10; //declaring variable x and assigning it the value of 1
